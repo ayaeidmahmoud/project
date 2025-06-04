50460f2021f956f800a9495f0d580d16e2dd18e1
shahd Hesham <shahedhesham13@gmail.com>
2025-06-04 13:15:20 +0300

    added sum function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello World! shahd branch")
+
+#sum function
+def add(a, b):
+    return a + b
+
+a = float(input("number1: "))
+b = float(input("number2: "))
+result = add(a, b)
+print(f"The result of the sum is: {result}")
