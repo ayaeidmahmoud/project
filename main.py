744d52988f23f8f6d756e39708a0d3aabd9d19b2
Ibrahimrafat <ibrahim.ra2fat96@gmail.com>
2025-06-04 13:39:46 +0300

    add new calculations by ibrahim

---
+++ main.py
@@ -0,0 +1,14 @@
+def sum(num1, num2):
+    """Returns the sum of two numbers."""
+    return num1 + num2
+def subtract(num1, num2):
+    """Returns the difference of two numbers."""
+    return num1 - num2
+def multiply(num1, num2):
+    """Returns the product of two numbers."""
+    return num1 * num2
+def divide(num1, num2):
+    """Returns the quotient of two numbers."""
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero.")
+    return num1 / num2
